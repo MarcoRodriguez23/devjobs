df8c429c257656bf5b91bcdf65b924e10b42133c
Marco Rodriguez <cv5233@innovaccion.mx>
2022-08-31 15:38:39 -0500

    cambios en vite a partir del blog de laracast.com

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+ 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+        laravel([
+            'resources/css/app.css', 
+            'resources/js/app.js',
+        ]),
     ],
 });
