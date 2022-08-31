9b71880105de293ca9e363dfd0f5ff52b3570f83
Marco Rodriguez <cv5233@innovaccion.mx>
2022-08-31 15:27:08 -0500

    agregando la clase de URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
